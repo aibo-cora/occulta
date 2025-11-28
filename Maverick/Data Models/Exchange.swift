32b22f32de8148392c4b0bdf5266b06ee85ff6a1
Yura Filatov <yura.fila@gmail.com>
2025-11-28 19:51:51 +0200

    Comments about the exchange model.

--- Maverick/Data Models/Exchange.swift
+++ Maverick/Data Models/Exchange.swift
@@ -12,6 +12,9 @@ struct Exchange: Codable {
     let id: String
     let token: Data
     let version: Version
+    /// Our long term identity public key.
+    ///
+    /// Used to verify our signature and in derivation of shared secret.
     let identity: Data?
     
     init(id: String, token: Data, version: Version, identity: Data? = nil) {
